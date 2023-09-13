03f8bf7806fe800ce751c7d0a79883c1ad5787f3
willcornforth <141945840+willcornforth@users.noreply.github.com>
2023-09-13 14:25:59 +0100

    Create initial 16x16 grid
    - All JS elements use static values for the time being.
    - Added loop to create containers within the grid-container.
    - Added basic styling for debug purposes.

--- sketchpad.js
+++ sketchpad.js
@@ -1,1 +1,15 @@
 console.log("Sketchpad 13/09/23")
+
+// Each grid box is 22px wide/tall.
+const gridSize = 16 * 16;
+const gridContainer = document.querySelector("#grid-container");
+
+for(let i = 0; i < gridSize; i++)
+{
+    const divRow = document.createElement("div");
+
+    divRow.id;
+    divRow.classList.add("grid-box");
+
+    gridContainer.appendChild(divRow);
+}
